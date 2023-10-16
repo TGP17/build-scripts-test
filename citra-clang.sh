f836a2054b4e079ddf92cce59b2e33bf32d3babb
TGP17 <86961834+TGP17@users.noreply.github.com>
2023-10-16 07:47:30 +0200

    Create citra-clang.sh

---
+++ citra-clang.sh
@@ -0,0 +1,36 @@
+sudo apt-get install -y \
+    build-essential \
+    libsdl2-dev \
+    libssl-dev \
+    gcc-11 \
+    g++-11 \
+    cpp-11 \
+# Qt 6
+    qt6-base-dev \
+    qt6-base-private-dev \
+    qt6-multimedia-dev \
+    qt6-l10n-tools \
+    qt6-tools-dev \
+    qt6-tools-dev-tools \
+    qt6-gtk-platformtheme \
+    qt6-documentation-tools \
+    qt6-wayland \
+# FFmpeg
+    ffmpeg \
+    libavcodec-dev \
+    libavdevice-dev \
+    libavfilter-dev \
+    libavformat-dev \
+    libswresample-dev \
+    libswscale-dev \
+# Tools
+    cmake \
+    p7zip-full \
+    wget \
+    unzip \
+    git \
+    ccache \
+    ninja-build \
+    glslang-dev \
+    glslang-tools \
+    file
