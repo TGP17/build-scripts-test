1d4f86a94efcc9de47b9b2d4dea53062bce04796
TGP17 <86961834+TGP17@users.noreply.github.com>
2023-10-16 07:52:50 +0200

    Update citra-clang.sh

--- citra-clang.sh
+++ citra-clang.sh
@@ -41,3 +41,12 @@ sudo apt-get install -y \
 git clone --recursive https://github.com/citra-emu/citra
 mkdir build
 cd build
+cmake .. -G Ninja \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_C_COMPILER_LAUNCHER=ccache \
+    -DCMAKE_CXX_COMPILER_LAUNCHER=ccache \
+    -DENABLE_QT_TRANSLATION=ON \
+    -DCITRA_ENABLE_COMPATIBILITY_REPORTING=ON \
+    -DENABLE_COMPATIBILITY_LIST_DOWNLOAD=ON \
+    -DUSE_DISCORD_PRESENCE=ON
+ninja
