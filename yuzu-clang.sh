7b7c0d2398a65d9cca8dec80c0e7881f49e121fd
TGP17 <86961834+TGP17@users.noreply.github.com>
2023-10-17 19:12:45 +0200

    Update yuzu-clang.sh

--- yuzu-clang.sh
+++ yuzu-clang.sh
@@ -131,3 +131,23 @@ export VCPKG_TOOLCHAIN_FILE=/home/yuzu/vcpkg/scripts/buildsystems/vcpkg.cmake
     #    nlohmann-json \
      #   zlib \
       #  zstd
+git clone --recursive https://github.com/yuzu-emu/yuzu-mainline
+cd yuzu-mainline
+mkdir build || true && cd build
+cmake .. \
+      -DBoost_USE_STATIC_LIBS=ON \
+      -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+      -DCMAKE_CXX_FLAGS="-march=x86-64-v2" \
+      -DCMAKE_CXX_COMPILER=clang++-15 \
+      -DCMAKE_C_COMPILER=clang-15 \
+      -DCMAKE_INSTALL_PREFIX="/usr" \
+      -DDISPLAY_VERSION=$1 \
+      -DENABLE_COMPATIBILITY_LIST_DOWNLOAD=ON \
+      -DENABLE_QT_TRANSLATION=ON \
+      -DUSE_DISCORD_PRESENCE=ON \
+      -DYUZU_ENABLE_COMPATIBILITY_REPORTING=${ENABLE_COMPATIBILITY_REPORTING:-"OFF"} \
+      -DYUZU_USE_BUNDLED_FFMPEG=ON \
+      -DYUZU_ENABLE_LTO=ON \
+      -GNinja
+
+ninja
