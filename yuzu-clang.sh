abe7a787e4ed9e1cc25bbd9dd50b48e45ae7e204
TGP17 <86961834+TGP17@users.noreply.github.com>
2023-10-18 00:56:36 +0200

    Update yuzu-clang.sh

--- yuzu-clang.sh
+++ yuzu-clang.sh
@@ -114,7 +114,8 @@ cmake .. \
       -DCMAKE_CXX_FLAGS="-march=x86-64-v2" \
       -DCMAKE_CXX_COMPILER=/usr/bin/clang++-15 \
       -DCMAKE_C_COMPILER=/usr/bin/clang-15 \
-      -DCMAKE_LINKER=/etc/bin/ld.lld \
+      -DCMAKE_C_FLAGS="-fuse-ld=lld" \
+      -DCMAKE_CXX_FLAGS="-fuse-ld=lld" \
       -DCMAKE_INSTALL_PREFIX="/usr" \
       -DDISPLAY_VERSION=$1 \
       -DENABLE_COMPATIBILITY_LIST_DOWNLOAD=ON \
