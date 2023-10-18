412aa590be83d4036e14482f4abda7567b59d9e5
TGP17 <86961834+TGP17@users.noreply.github.com>
2023-10-18 12:18:22 +0200

    Update yuzu-clang.sh

--- yuzu-clang.sh
+++ yuzu-clang.sh
@@ -1,3 +1,111 @@
+export BOOST_VER=1_79_0
+export CLANG_VER=15
+export CMAKE_VER=3.22.6
+export GCC_VER=12.2.0
+export GNU_BIN_VER=2.40
+export QT_PKG_VER=515
+export QT_VER=5.15.2
+export UBUNTU_VER=focal
+
+sudo add-apt-repository -y ppa:savoury1/build-tools
+sudo add-apt-repository -y ppa:savoury1/display
+sudo add-apt-repository -y ppa:savoury1/ffmpeg4
+sudo add-apt-repository -y ppa:savoury1/gcc-defaults-11
+sudo add-apt-repository -y ppa:savoury1/graphics
+sudo add-apt-repository -y ppa:theofficialgman/gpu-tools
+sudo add-apt-repository -y ppa:savoury1/multimedia
+sudo add-apt-repository -y ppa:git-core/ppa
+sudo add-apt-repository -y ppa:beineri/opt-qt-${QT_VER}-${UBUNTU_VER}
+
+sudo apt-get update
+sudo apt-get full-upgrade -y
+sudo apt-get install -y \
+    apt-utils \
+    ca-certificates \
+    gnupg \
+    software-properties-common \
+    unzip \
+    wget \
+    xz-utils \
+    build-essential \
+    ccache \
+    git \
+    libgl-dev \
+    liblz4-dev \
+    libpulse-dev \
+    libudev-dev \
+    libssl-dev \
+    libtool \
+    libwayland-dev \
+    ninja-build \
+    pkg-config \
+    zlib1g-dev \
+    appstream \
+    desktop-file-utils \
+    file \
+    libfile-mimeinfo-perl \
+    patchelf \
+    zsync \
+    libdrm-dev \
+    libva-dev \
+    libx11-dev \
+    libxext-dev \
+    nasm \
+    autoconf \
+    automake \
+    libtool \
+    libudev-dev \
+    gpg-agent \
+    curl \
+    git \
+    glslang-dev \
+    glslang-tools \
+    libhidapi-dev \
+    qt${QT_PKG_VER}base \
+    qt${QT_PKG_VER}tools \
+    qt${QT_PKG_VER}wayland \
+    qt${QT_PKG_VER}multimedia \
+    qt${QT_PKG_VER}x11extras \
+    mold \
+    zip
+    
+# Install Clang from apt.llvm.org
+wget https://apt.llvm.org/llvm.sh
+chmod +x llvm.sh
+sudo ./llvm.sh 15 all
+sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-15 150
+sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-15 150
+ln -s /etc/bin/ld.lld /usr/bin/ld
+
+# Install CMake from upstream
+cd /tmp && \
+    wget --no-verbose https://github.com/Kitware/CMake/releases/download/v${CMAKE_VER}/cmake-${CMAKE_VER}-linux-x86_64.tar.gz && \
+    tar xvf cmake-${CMAKE_VER}-linux-x86_64.tar.gz && \
+    sudo cp -rv cmake-${CMAKE_VER}-linux-x86_64/* /usr && \
+    rm -rf cmake-*
+
+# Install Boost from yuzu-emu/ext-linux-bin
+cd /tmp && \
+    wget --no-verbose https://github.com/yuzu-emu/ext-linux-bin/raw/main/boost/boost-${BOOST_VER}.tar.xz && \
+    tar xvf boost-${BOOST_VER}.tar.xz && \
+    sudo cp -rv boost-${BOOST_VER}/usr / && \
+    rm -rf boost*
+
+# Install vcpkg and required dependencies for yuzu
+git clone --depth 1 https://github.com/Microsoft/vcpkg.git &&\
+    cd vcpkg &&\
+    ./bootstrap-vcpkg.sh &&\
+    ./vcpkg install \
+        catch2 \
+        fmt \
+     lz4 \
+        nlohmann-json \
+        zlib \
+        zstd
+        
+# Tell CMake to use vcpkg when looking for packages
+export VCPKG_TOOLCHAIN_FILE=/tmp/vcpkg/scripts/buildsystems/vcpkg.cmake
+
 # Compile yuzu
 git clone --recursive https://github.com/yuzu-emu/yuzu-mainline
 cd yuzu-mainline
