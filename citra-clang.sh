d05ac1360e9fa7b7d25c8cec7c23f80ce60128a7
TGP17 <86961834+TGP17@users.noreply.github.com>
2023-10-22 22:12:35 +0200

    Update citra-clang.sh

--- citra-clang.sh
+++ citra-clang.sh
@@ -44,8 +44,8 @@ wget https://apt.llvm.org/llvm.sh
 chmod +x llvm.sh
 sudo ./llvm.sh 15 all
 
-git clone --recursive https://github.com/citra-emu/citra
-cd citra
+git clone --recursive https://github.com/citra-emu/citra-nightly
+cd citra-nightly
 mkdir build
 cd build
 cmake .. -G Ninja \
